dd5a0ff15c751800f088dc7f66b277dcaabc1d2b
Pramod Singh <askpramod@users.noreply.github.com>
2018-12-24 15:06:07 +0530

    use cloud-init.txt

--- setup.sh
+++ setup.sh
@@ -7,8 +7,7 @@ read -p 'KeyVault name: ' kvname
 #read -p 'Storage Account name: ' saname
 
 
-az group create --name $rgname --location $region && az vm create -n $vmname -g $rgname --image UbuntuLTS --admin-username azureadmin --generate-ssh-keys --custom-data bs-init.txt
-az vm create -n $vmname -g $rgname --image UbuntuLTS --admin-username azureadmin --generate-ssh-keys --custom-data cloud-init.txt
+az group create --name $rgname --location $region && az vm create -n $vmname -g $rgname --image UbuntuLTS --admin-username azureadmin --generate-ssh-keys --custom-data cloud-init.txt
 az vm open-port --port 80 --resource-group $rgname --name $vmname
 az vm identity assign --name $vmname --resource-group $rgname
 az keyvault create --name $kvname --resource-group $rgname --enabled-for-deployment true
