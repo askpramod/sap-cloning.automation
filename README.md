4dc3a8273849dec1bc5fbf64d108ef91bccd15fb
Pramod Singh <pramod.singh@kiwitech.com>
2018-12-27 13:42:06 +0530

    a few other modification for schema migration

--- README.md
+++ README.md
@@ -7,13 +7,17 @@ Tools used -
 I used azure-cli and cloud-init as these simple yet powerful tools worked for efficient provisioning and setup.
 
 Azure keyvault - I explored it, understand it but facing a challenge in integrating it with the app. I am using two npm modules for integration - azure-keyvault (a SDK) and ms-rest-azure (for authentication using Managed Service Identity or service to azure-service authentication).
-
+Facing issue reportetd at - 
+https://github.com/Azure-Samples/app-service-msi-keyvault-node/issues/5
+https://github.com/Azure-Samples/app-service-msi-keyvault-node/issues/4
 
 Steps to deploy this application?
 
 - Clone this repo
 - Login to azure-cli with "az login"
-- Run setup.sh with chmod +x setup.sh && bash setup.sh
+- Run setup.sh with "bash setup.sh"
 - Copy the VM IP address from output of above command to browse the app.
 
 You can ssh on the VM with azureadmin@VM-IP-Address
+
+
--- setup.sh
+++ setup.sh
@@ -4,11 +4,12 @@ read -p 'Resource Group name: ' rgname
 read -e -p 'Resource Group region: ' -i 'centralindia' region 
 read -p 'VM name: ' vmname
 read -p 'KeyVault name: ' kvname
-#read -p 'Storage Account name: ' saname
+vm-object-id=$(az ad sp list  --display-name $vmname |jq -r '.[].objectId')
 
-
-az group create --name $rgname --location $region && az vm create -n $vmname -g $rgname --image UbuntuLTS --admin-username azureadmin --generate-ssh-keys --custom-data cloud-init.txt
+az group create --name $rgname --location $region
+az vm create -n $vmname -g $rgname --image UbuntuLTS --admin-username azureadmin --generate-ssh-keys --custom-data cloud-init.txt
 az vm open-port --port 80 --resource-group $rgname --name $vmname
 az vm identity assign --name $vmname --resource-group $rgname
 az keyvault create --name $kvname --resource-group $rgname --enabled-for-deployment true
 az keyvault secret set --vault-name $kvname --name "password" --value "secret"
+az keyvault set-policy --name $kvname --resource-group $rgname --object-id $vm-object-id --secret-permissions get 
